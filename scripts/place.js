afb57ed4e8919976e46f682b29fb509b180e9d58
benpar23 <par21015@byui.edu>
2024-11-18 23:17:25 -0500

    almost finished with place webpage!

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,20 @@ const today = new Date();
 currentyear.innerHTML = today.getFullYear();
 
 lastModified.innerHTML = document.lastModified;
+
+const windChill = document.querySelector(".chill");
+
+const temp = parseInt(document.querySelector(".temp").textContent);
+
+const wind = parseInt(document.querySelector(".wind").textContent);
+
+function calculateWindChill(temp, wind) {
+    return 13.12 + 0.6215 * temp - 11.37 * wind ** 0.16 + 0.3965 * temp * wind ** 0.16;
+}
+
+if (temp <= 10 && wind > 4.8) {
+    windChill.textContent = `${Math.round(calculateWindChill(temp, wind))} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
+
