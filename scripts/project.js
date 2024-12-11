5c123ff30bcebcc1b322130c5d481e819961b12b
benpar23 <par21015@byui.edu>
2024-12-11 17:34:10 -0500

    started building out API JS for final website project

--- scripts/project.js
+++ scripts/project.js
@@ -7,3 +7,23 @@ const today = new Date();
 currentyear.innerHTML = today.getFullYear();
 
 lastModified.innerHTML = document.lastModified;
+
+let pokemonList = [];
+
+let teamList = [];
+
+const getPokemon = async (pokemon) => {
+    
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (response.ok){
+        const characters = await response.json();
+
+        if (Array.isArray(characters.data)){
+            pokemonList = characters.data;
+    } else {
+        pokemonList = [characters.data];
+    }}
+
+    displayDisneyChar(disneyList);    
+}
